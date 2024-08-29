0cdbfb5fc551a1aa81898471a9f88531beb408cd
im-sanny <raselparvezsanny@gmail.com>
2024-08-29 14:25:19 +0600

    update: updated the template and added image generator

--- ai-client/src/firebase/firebase.config.js
+++ ai-client/src/firebase/firebase.config.js
@@ -1,16 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAyd0c5kt2maFnfZP4aBShwgI9CKM6yyZM",
-  authDomain: "chef-note.firebaseapp.com",
-  projectId: "chef-note",
-  storageBucket: "chef-note.appspot.com",
-  messagingSenderId: "789656932914",
-  appId: "1:789656932914:web:a28d9f2dd2ba29eff1622b",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
--- ai-client/src/pages/Generate.jsx
+++ ai-client/src/pages/Generate.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import Title from "../components/Title";
+import Loading from "./Loading";
 
 const Generate = () => {
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
   const handleSubmit = (e) => {
+    setLoading(true);
     e.preventDefault();
     console.log(e.target.prompt.value);
     const form = new FormData();
@@ -10,16 +15,23 @@ const Generate = () => {
     fetch("https://clipdrop-api.co/text-to-image/v1", {
       method: "POST",
       headers: {
-        "x-api-key": VITE_AI_APIKEY,
+        "x-api-key": import.meta.env.VITE_AI_APIKEY,
       },
       body: form,
     })
       .then((response) => response.arrayBuffer())
       .then((buffer) => {
         console.log(buffer);
+        const blob = new Blob([buffer], { type: "image/jpeg" });
+        const image_url = URL.createObjectURL(blob);
+        setImages([image_url, ...images]);
         // buffer here is a binary representation of the returned image
+        setLoading(false);
       });
   };
+  if (loading) {
+    <Loading></Loading>;
+  }
   return (
     <div className="container">
       <Title>Generate Paintings</Title>
@@ -36,6 +48,13 @@ const Generate = () => {
         />
         <button className="btn btn-primary ">Generate</button>
       </form>
+      <div className="grid lg:grid-cols-2 gap-5">
+        {images.map((images) => (
+          <div>
+            <img className="border-8 p-5 w-full" src={images} alt="" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
